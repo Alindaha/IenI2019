f9552045c4fd7d0a32b6d5545728854b39720d58
Alindaha <alindaharmanny@gmail.com>
2019-11-25 20:36:26 +0000

    Opdracht16

--- JS/H1/H1O16.js
+++ JS/H1/H1O16.js
@@ -1,4 +1,6 @@
 var diameter = 300;
+var bY=450;
+var gY=0;
 
 function setup() {
   canvas = createCanvas(450,450);
@@ -11,4 +13,10 @@ function draw() {
   background(255,255,255,1);
   fill(255,0,0,0.5);
   ellipse(width / 2,height / 2,diameter);
+  fill(0,255,0,0.5);
+  ellipse(width / 2,gY,diameter);
+  fill(0,0,255,0.5);
+  ellipse(width / 2,bY,diameter);
+  bY-=1;
+  gY+=1;
 }
