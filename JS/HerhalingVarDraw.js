e588c2252f44d74a65a1b3df9f2febda586f719d
Alindaha <alindaharmanny@gmail.com>
2019-12-02 13:24:35 +0000

    HerhalingOpdr

---
+++ JS/HerhalingVarDraw.js
@@ -0,0 +1,14 @@
+function setup() {
+  canvas = createCanvas(450,450);
+  background('silver');
+  canvas.parent('processing');
+  //noLoop();
+}
+
+function draw() {
+  noStroke();
+  fill('deepskyblue');
+  ellipse(450,450,400);
+  fill('black');
+  ellipse(100,100,100);
+}
---
+++ JS/HerhalingVarDrawU.js
@@ -0,0 +1,20 @@
+var hoogte = 100;
+var diameter = 400;
+
+function setup() {
+  canvas = createCanvas(450,450);
+  background('silver');
+  canvas.parent('processing');
+  //noLoop();
+}
+
+function draw() {
+  background('silver');
+  noStroke();
+  fill('deepskyblue');
+  ellipse(450,450,diameter);
+  fill('black');
+  ellipse(100,hoogte,100);
+  hoogte++;
+  diameter++;
+}
