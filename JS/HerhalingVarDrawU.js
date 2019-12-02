6278507186b2b9a46cd0b7aaa970e60934d10561
Alindaha <alindaharmanny@gmail.com>
2019-12-02 21:45:37 +0000

    HerhalingsOpdrDef

--- JS/HerhalingVarDrawU.js
+++ JS/HerhalingVarDrawU.js
@@ -1,5 +1,5 @@
 var hoogte = 100;
-var diameter = 400;
+var diameter = 0;
 
 function setup() {
   canvas = createCanvas(450,450);
@@ -13,9 +13,12 @@ function draw() {
   noStroke();
   fill('deepskyblue');
   ellipse(450,450,diameter);
+  fill('grey');
+  ellipse(0,0,diameter);
   fill('black');
   ellipse(100,hoogte,100);
   hoogte = constrain(hoogte,100,height-50);
+  diameter = constrain(diameter,0,450);
   hoogte++;
   diameter++;
 }
