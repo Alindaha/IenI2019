edd0aa3a3857c7ee5311a97339882b364612eded
Alindaha <alindaharmanny@gmail.com>
2019-12-10 21:48:35 +0000

    Opdracht31Gek

--- JS/H1/H1O31.js
+++ JS/H1/H1O31.js
@@ -1,16 +1,23 @@
 kleur='white';
 
 function setup() {
-  canvas = createCanvas(451,451);
+  canvas = createCanvas(501,501);
   canvas.parent('processing');
   background('coral');
+  noLoop();
 }
 
 function draw() {
-  for (var rij = 0;rij < 450;rij += 50) {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
+  for (var rij = 0;rij < 501;rij += 50) {
+    for (var kolom = 0;kolom < 501;kolom += 50) { 
       fill(kleur);
       rect(kolom,rij,50,50);
+      if(kleur=='white'){
+          kleur='red';
+      } else {
+          kleur='white';
+      }
     }
+    
   }
 }
