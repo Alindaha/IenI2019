fd04c133caeb896b550b3e6e91214d1e30b468ec
Alindaha <alindaharmanny@gmail.com>
2020-01-07 19:05:44 +0000

    SpelKlaar

--- JS/Vwo1.js
+++ JS/Vwo1.js
@@ -1,96 +1,98 @@
 var xJOS=225;
 var yJOS=425;
 var level=1;
-var xPijl;
 var yPijl=375;
 var xBal=100;
 var yBal=100;
 var dBal=50;
 var sBal=5;
-var afstand;
+var pAfstand;
+var jAfstand;
 var voorraad;
-var scores;
-
 
 function setup() {
-  canvas = createCanvas(450,450);
-  background('silver');
-  canvas.parent('processing');
-  dBal=random(30,80);
-  xBal=random(dBal/2,width-dBal/2);
-  yBal=dBal/2;
-  voorraad=1;
-  //noLoop();
+    canvas = createCanvas(450,450);
+    background('silver');
+    canvas.parent('processing');
+    dBal=random(30,80);
+    xBal=random(dBal/2,width-dBal/2);
+    yBal=dBal/2;
+    voorraad=3;
+    textFont("Verdana");
+    textSize(20);
 }
 
 function draw() {
-  background('silver');
-  fill('black');
-  text("Level: "+level,10,440);
-  scores=new Array(5,10,6);
-  tekenJos(xJOS,yJOS);
-  tekenBal(xBal,yBal,dBal);
-  tekenVoorraad(voorraad);
-  if(level==1){
-      text("New game!",50,50);
-  }
-  if (keyIsDown(LEFT_ARROW)) {
-    xJOS=xJOS-2;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    xJOS=xJOS+2;
-  }
-  if(keyIsDown(32)){
-      tekenPijl(xJOS,yPijl);  
-      yPijl--;
-      if(yPijl==0 && voorraad==0){
-          scores.push(level);
-          level=1;
-          dBal=random(30,80);
-          xBal=random(dBal/2,width-dBal/2);
-          yBal=dBal/2;
-          sBal=5;
-          voorraad=1;
-          yPijl=375;
-      }
-      if(yPijl==0){
-          voorraad--;
-          yPijl=375;
-      }
-  }
-  if(keyIsDown(32)==false){
-      yPijl=375;
-  }
-  if(xBal>(dBal/2) && xBal<(width-(dBal/2))){
-    xBal=xBal+sBal;
-  } else {
-      sBal*=-1;
-      xBal=xBal+sBal;
-      yBal=yBal+level;
-  }
-  afstand=dist(xJOS,yPijl,xBal,yBal);
-  if(afstand<=(dBal/2)){
-      level++;
-      dBal=random(10,80);
-      xBal=random(dBal/2,width-dBal/2);
-      yBal=dBal/2;
-      sBal++;
-      yPijl=375;
-  }
-  if(keyIsDown(81)){
-      eindScherm();
-  }
+    background('silver');
+    fill('black');
+    tekenJos(xJOS,yJOS);
+    tekenBal(xBal,yBal,dBal);
+    tekenVoorraad(voorraad);
+    text("Level "+level,10,440);
+    gebruikersBesturing();
+    if(xBal>(dBal/2) && xBal<(width-(dBal/2))){
+        xBal=xBal+sBal;
+        yBal=yBal+0.1*level;
+    } else {
+        sBal*=-1;
+        xBal=xBal+sBal;
+    }
+    pAfstand=dist(xJOS,yPijl,xBal,yBal);
+    if(pAfstand<=(dBal/2)){
+        level++;
+        dBal=random(10,80);
+        xBal=random(dBal/2,width-dBal/2);
+        yBal=dBal/2;
+        sBal++;
+        yPijl=375;
+    }
+    jAfstand=dist(xJOS,yJOS,xBal,yBal);
+    if(jAfstand<=(dBal+25)|| yBal>=(height-(dBal/2))){
+        if(voorraad>0){
+            voorraad--;
+        } else {
+            eindScherm(level-1);
+        }
+        dBal=random(10,80);
+        xBal=random(dBal/2,width-dBal/2);
+        yBal=dBal/2;
+        yPijl=375;
+    }
 }
 
-function eindScherm(){
+function gebruikersBesturing(){
+    if (keyIsDown(LEFT_ARROW)) {
+        xJOS=xJOS-2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        xJOS=xJOS+2;
+    }
+    if(keyIsDown(32)){
+        tekenPijl(xJOS,yPijl);  
+        yPijl--;
+        if(yPijl<=0 && voorraad==0){
+            eindScherm(level-1);
+        }
+        if(yPijl<=0){
+            voorraad--;
+            yPijl=375;
+        }
+        if(keyIsDown(UP_ARROW)){
+            yPijl-=2;
+        }
+    }
+    if(keyIsDown(32)==false){
+      yPijl=375;
+    }
+    xJOS=constrain(xJOS,25,425);
+}
+
+function eindScherm(score){
     push();
+    textSize(50);
     background('black');
     fill('pink');
-    text("Je wilt stoppen...\nDit zijn de scores:\n", 50,50);
-    for(var i=0;i<scores.length;i++){
-        fill('pink');
-        text("Poging "+(i+1)+": "+scores[i]+scores.length,50,((i*15)+100));
-    }
+    text("Game over! \nJe score is " + score, 75,200);
     noLoop();
     pop();
 }
@@ -124,23 +126,23 @@ function tekenPijl(x,h){
 }
 
 function tekenJos(x,y,s) {
-  push();
-  translate(x,y);
-  scale(s); 
-  noStroke();
-  fill('indianred');
-  ellipse(0,0,50);
-  fill('slategray');
-  ellipse(-7,-10,17);
-  ellipse(7,-10,17);
-  fill('white');
-  ellipse(-7,-8,7,13);
-  ellipse(7,-8,7,13);
-  fill('orange');
-  ellipse(0,3,17);
-  stroke('slategray');
-  strokeWeight(3);
-  fill('white');
-  arc(0, 13, 26, 13, 0, PI, CHORD);
-  pop();
+    push();
+    translate(x,y);
+    scale(s); 
+    noStroke();
+    fill('indianred');
+    ellipse(0,0,50);
+    fill('slategray');
+    ellipse(-7,-10,17);
+    ellipse(7,-10,17);
+    fill('white');
+    ellipse(-7,-8,7,13);
+    ellipse(7,-8,7,13);
+    fill('orange');
+    ellipse(0,3,17);
+    stroke('slategray');
+    strokeWeight(3);
+    fill('white');
+    arc(0, 13, 26, 13, 0, PI, CHORD);
+    pop();
 }
