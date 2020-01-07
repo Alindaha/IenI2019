dc874b6e508eaa52dd0b80c602f27aa5e7197463
Alindaha <alindaharmanny@gmail.com>
2020-01-07 20:29:23 +0000

    VWOTheoretischInTest

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,40 @@
 function setup() {
-  canvas = createCanvas(450,450);
-  background('silver');
+  canvas = createCanvas(1500,450);
+  background('white');
   canvas.parent('processing');
   //noLoop();
 }
 
 function draw() {
+    //A
   noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+  fill('black');
+  ellipse(150,250,200);
+  fill('white');
+  stroke('gray');
+  ellipse(200,250,200);
+
+  //B
+  noStroke();
+  fill('black');
+  ellipse(500,250,200);
+  fill('white');
+  stroke('gray');
+  ellipse(450,250,200);
+
+  //C
+  noStroke();
+  fill('black');
+  ellipse(800,250,200);
+  fill('white');
+  stroke('gray');
+  ellipse(800,250,150);
+
+  //D
+  noStroke();
+  fill('black');
+  ellipse(1200,250,200);
+  fill('white');
+  stroke('gray');
+  ellipse(800,250,250);
 }
