b29958d82a44d2ee15e088910b79cbcb6eed8e68
Alindaha <alindaharmanny@gmail.com>
2020-01-13 11:39:34 +0000

    update13

--- JS/DierLeerlingen.js
+++ JS/DierLeerlingen.js
@@ -10,33 +10,23 @@ function setup() {
 }
 
 function draw() {
-  background('silver');
-  translate(80,100);
-  for(var i=0; i<4;i++){
-    if(i==0){
-        kleur='gray';
-    } else {
-        kleur='white';
-    }
-    tekenDier(xDier,yDier,kleur);
+    background('silver');
+    translate(80,100);
+    tekenDier(xDier,yDier);
     translate(110,80);
-  }
-  xDier++;
 }
 
-function tekenDier(x,y,kleur){
+function tekenDier(x,y){
     noStroke();
-    fill(kleur);
+    fill('white');
     ellipse(x-30,y-30,30,80);
     ellipse(x+30,y-30,30,80);
     fill('pink');
     ellipse(x-30,y-25,20,60);
     ellipse(x+30,y-25,20,60);
-    fill(kleur);
+    fill('white');
     ellipse(x,y,100);
     fill('black');
     ellipse(x-20,y-10,15,10);
     ellipse(x+20,y-10,15,10);
-    fill('brown');
-    triangle(x-10,y+10,x+10,y+10,x,y+20);
 }
