c2063bca41382e2a4edaa2d0c7d5fd87973c86c7
Alindaha <alindaharmanny@gmail.com>
2020-01-14 10:09:22 +0000

    test

--- JS/test.js
+++ JS/test.js
@@ -2,41 +2,14 @@ function setup() {
   canvas = createCanvas(1500,450);
   background('white');
   canvas.parent('processing');
-  //noLoop();
+  noLoop();
 }
 
 function draw() {
-    //A
-  fill('black');
-  rect(100,150,200,200);
-  fill('white');
-  stroke('gray');
-  triangle(100,150,300,150,200,250);
-  triangle(100,350,300,350,200,250);
-
-  //B
-  fill('black');
-  rect(350,150,200,200);
-  fill('white');
-  stroke('gray');
-  triangle(350,150,550,150,450,350);
-  triangle(350,350,550,350,450,150);
-
-  //C
-  fill('black');
-  rect(600,150,200,200);
-  fill('white');
-  stroke('gray');
-  triangle(600,150,600,350,700,250);
-  triangle(800,150,800,350,700,250);
-
-  //D
-  fill('black');
-  rect(850,150,200,200);
-  fill('white');
-  stroke('gray');
-  triangle(850,150,850,350,1050,250);
-  triangle(1050,150,1050,350,850,250);
-
-
+    var aantal=4;
+    sf=1;
+    for(var a=1;a<=aantal;a++){
+        sf=sf+a/2;
+    }
+    text("sf= "+sf,100,50);
 }
